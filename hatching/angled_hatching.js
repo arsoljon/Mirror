2209adc85a693807ea8031cd35e8df2978665f00
arsoljon <jdanarsola@gmail.com>
2024-08-01 04:09:42 -0500

    completed the angled hatching class.

--- hatching/angled_hatching.js
+++ hatching/angled_hatching.js
@@ -3,9 +3,11 @@ export default class Angled_Hatching{
         this.width = w;
         this.length = l;
         this.allHatches = [];
-        this.maxCount = 2;
+        this.maxCount = 10;
+        this.offset = 200;
         for(let i = 0; i <= this.maxCount; i++){
-            let hatch = new Hatch(p5, (this.width/this.maxCount) * i, this.length);
+          let startX = ((this.width+this.offset) / this.maxCount)*i;
+            let hatch = new Hatch(p5, startX, 0, this.offset);
             this.allHatches.push(hatch);    
         }
     }
@@ -17,14 +19,15 @@ export default class Angled_Hatching{
 }
 
 class Hatch{
-    constructor(p5, width_, length_){
+    constructor(p5, width_, length_, offset_){
         this.x = width_;
         this.y = length_;
+        this.offset = offset_;
         this.hatch = [];
-        this.maxCount = 50;
+        this.maxCount = 20;
         this.horizontal_speed = 0;
         for(let i = 0; i < this.maxCount; i++){
-            let start = p5.createVector(this.x, 0);
+            let start = p5.createVector(this.x-this.offset, 1);
             let end = p5.createVector(this.x, this.y);
             let particle = new Particle(p5, start, end);
             this.hatch.push(particle);
@@ -45,14 +48,14 @@ class Particle{
     this.diff = this.start.x - this.start.y
     this.start.y = p5.random(0,this.end.y) + this.diff;
     this.start.x = this.start.y + this.diff;
-    this.size = p5.createVector(2,10);
+    this.size = p5.createVector(1,60);
     this.loc = p5.createVector(this.start.x, this.start.y);
     this.angle = 45;
     this.xSpeed = p5.random(1,2);
     this.ySpeed = this.xSpeed;
-    this.alpha = p5.random(0,255);
+    this.alpha = p5.random(0,20);
     this.fill = [101, 101, 98];
-    this.alphaChange = 0.9;
+    this.alphaChange = p5.random(0.5, 1);
   }
   drawParticle(p5,h_speed){
     p5.push();
@@ -61,17 +64,17 @@ class Particle{
     p5.fill(this.fill[0], this.fill[1], this.fill[2], this.alpha);
     p5.ellipse(0,0,this.size.x, this.size.y);
     p5.pop();
-    this.resetParticle();
+    this.resetParticle(p5);
   }
   resetParticle(p5,h_speed){
     this.loc.x += this.xSpeed;
     this.loc.y += this.ySpeed;
     this.alpha -= this.alphaChange;
-    if(this.loc.y > this.end.y + this.size.y){
+    if(this.loc.y > p5.height + this.size.y){
       this.loc.y = 0 - this.size.y;
       this.loc.x = 0 + this.diff - this.size.y;
     }
-    if(this.loc.x > this.end.x +  this.size.y){
+    if(this.loc.x > p5.width +  this.size.y){
       this.loc.y = 0 - this.size.y;
       this.loc.x = 0 + this.diff - this.size.y;
     }
